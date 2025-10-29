3e4e86e2540f1d700d0abd3894b5de054b174e5d
tuanthe <tuandthe173140@fpt.edu.vn>
2025-10-29 16:34:19 +0700

    Fix: Set CI=false to allow warnings during build

--- src/Frontend/src/App.tsx
+++ src/Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FullHistoryPage from './pages/FullHistoryPage';
